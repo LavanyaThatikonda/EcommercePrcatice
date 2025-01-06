19e11f7b57f770f0f80ca19996e6a082d6a09b39
lathatik <lavanyereddyt437@gmail.com>
2025-01-06 13:41:54 +0530

    initial commit

---
+++ src/test/java/com/example/EcommercePractice/EcommercePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcommercePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
